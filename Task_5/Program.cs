d6edfd190b743454742cd0cdff7a0fa2b5866c22
Данил <dnazmut@mail.ru>
2025-01-28 14:40:24 +0500

    происходит вызов connection string

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -1,9 +1,14 @@
 using Test1.Services;
 var builder = WebApplication.CreateBuilder(args);
 
+string connectionString = @"Server=DESKTOP-QR21RPK;Database=Task_5;Trusted_Connection=True;";
+
 
 IServiceCollection allServices = builder.Services; // коллекция сервисов
-IServiceCollection serviceCollection = allServices.AddTransient<IProductService, ProductService>(provider => new ProductService());
+
+
+IServiceCollection serviceCollection = allServices.AddTransient<IProductService, ProductService>(provider => new ProductService(connectionString));
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
