a7a226fce05f06c6c67f0162fa1e72f6adc05b4f
Данил <dnazmut@mail.ru>
2025-01-28 22:33:16 +0500

    метод get user изменен на асинхронный

--- Task_5/Services/ProductService.cs
+++ Task_5/Services/ProductService.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Dapper;
-
 namespace Test1.Services
 {
     public class ProductService : IProductService
@@ -12,18 +11,19 @@ namespace Test1.Services
         
 
         private readonly string _connectionString;
+
         public ProductService(string connectionString){
 
             _connectionString = connectionString;
         }
 
-        public List<User> GetUsers()
+        public async Task<IEnumerable<User>> GetUsersAsync()
         {
             using (var db = new SqlConnection(_connectionString))
             {
-                return db.Query<User>("SELECT Id, Name, Age FROM Users").ToList();
+                var users = await db.QueryAsync<User>("SELECT Id, Name, Age FROM Users");
+                return users.ToList();
             }
-
         }
         public void AddUser(User user){
             using (var db = new SqlConnection(_connectionString)){
@@ -40,7 +40,6 @@ namespace Test1.Services
             using(var db = new SqlConnection(_connectionString)){
                 var sqlQuery = "DELETE FROM Users WHERE Id = @Id";
                 db.Execute(sqlQuery,new { id });
-
             }
         }
 
