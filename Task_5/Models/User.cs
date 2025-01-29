fc3a4b4563e3021e7d23cfeea36fed4ed469c727
Данил <dnazmut@mail.ru>
2025-01-29 14:14:21 +0500

    добавлен json settings

--- Task_5/Models/User.cs
+++ Task_5/Models/User.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
+using System.Text.Json.Serialization;
+
 namespace Test1
 {
     public class User
     {
+        [JsonPropertyName("IdUser")]
         public int Id{get; set;}
+        [JsonPropertyName("NameUser")]
         public string Name{get; set;}
+
+
+        [JsonIgnore]
+        public string Surname{get; set;}
+        [JsonIgnore]
         public int Age{get; set;}
+        [JsonIgnore]
+        public string Email{get; set;}
+        
     }
 }
