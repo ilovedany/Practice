20ffa6a7f3a36f0a8c886802483c0dabe73bdeca
Данил <dnazmut@mail.ru>
2025-01-29 17:50:11 +0500

    добавлено новое задание

---
+++ Task_6/Program.cs
@@ -0,0 +1,22 @@
+﻿using Test1;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+class MainProgram{
+
+    HttpClient httpClient = new HttpClient();
+    public static async Task HttpMethodAsync(HttpClient httpClient){
+
+        var data = await httpClient.GetFromJsonAsync<List<User>>("http://localhost:5009/getUsers");
+        foreach(var d in data){
+            Console.WriteLine($"Id = {d.Id}, Name = {d.Name}");
+        }
+    }
+
+    static async Task Main(string[] args){
+
+        HttpClient httpClient = new HttpClient();
+        await HttpMethodAsync(httpClient);
+
+    }
+}
