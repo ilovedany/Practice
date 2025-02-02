31a39245a05d3761912fbd98eefef4aeef96160c
Данил <dnazmut@mail.ru>
2025-02-02 23:33:41 +0500

    Добавлено задание

---
+++ Task_7/Task_7_Form/Models/UserAdress.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpForm.Models
+{
+    public class UserAdress
+    {
+        public int Adresses_Id { get; set; }
+
+        public string City { get; set; }
+
+        public string Adress { get; set; }
+
+    }
+}
---
+++ Task_7/Task_7_Form/ViewModels/PostUserView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpForm.Models;
+namespace HttpForm.ViewModels
+{
+    public class PostUserView : ObservableCollection<User>
+    {
+        public PostUserView()
+        {
+            HttpClient httpClient = new HttpClient();
+            var userDict =
+
+            new Dictionary<string, string>
+            {
+              {"Name","Данил"},
+              {"Surname","Назмутдинов"},
+              {"Age","19"},
+              {"Email","awda@mail.ru"}
+
+            };
+            try
+            {
+                var user = Task.Run(() => HttpRequest.HttpUser.PostUserAsync(httpClient, "http://localhost:5009/addUser", userDict)).Result;
+                Add(user);
+            }
+
+            catch (Exception ex)
+            { }
+        }
+
+        
+
+
+    }
+}
