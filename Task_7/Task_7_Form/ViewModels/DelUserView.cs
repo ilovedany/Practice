ee96d8ba44338430236a6af11bc2a35af0357b48
Данил <54797756+ilovedany@users.noreply.github.com>
2025-02-03 16:58:08 +0500

    добавлена datagridview для таблицы SpecialistRank

---
+++ Task_7/Task_7_Form/ViewModels/DelUserView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpForm.Models;
+
+namespace HttpForm.ViewModels
+{
+    public class DelUserView
+    {
+        public void DelUserMethod()
+        {
+            HttpClient httpClient = new HttpClient();
+            Task.Run(() => HttpRequest.HttpUser.DelUserAsync(httpClient, "http://localhost:5009/deleteUser", 9));
+        }
+        
+    }
+}
---
+++ Task_7/Task_7_Form/ViewModels/PostUserView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HttpForm.Models;
+namespace HttpForm.ViewModels
+{
+    public class PostUserView
+    {
+        public void PostUserMethod()
+        {
+            HttpClient httpClient = new HttpClient();
+            var userDict =
+
+            new Dictionary<string, string>
+            {
+              {"Name","Данил"},
+              {"Surname","Назмутдинов"},
+              {"Age","19"},
+              {"Email","awda@mail.ru"}
+
+            };        
+            Task.Run(() => HttpRequest.HttpUser.PostUserAsync(httpClient, "http://localhost:5009/addUser", userDict));
+        
+        }
+
+    }
+}
